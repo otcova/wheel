373690fdbbb09ecf746e50a448e9d011820e9f4c
otcova <otgercomas@gmail.com>
2023-02-09 09:04:34 +0100

    Main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,67 @@
-<script>
-    import Wheel from "./Wheel/wheel.svelte";
+<script lang="ts">
+    import { browser } from "$app/environment";
+    import ItemList from "./ItemList.svelte";
+    import Wheel from "./Wheel.svelte";
+
+    if (browser) {
+        addEventListener("contextmenu", (event) => event.preventDefault());
+    }
+    
 </script>
 
-<Wheel/>
+<div class="wheel_layout">
+    <ItemList />
+    <Wheel />
+</div>
 
+<style>
+    :global(html),
+    :global(body) {
+        width: 100%;
+        height: 100%;
+        background-color: #625a75;
+        margin: 0;
+        padding: 0;
+        overflow-x: hidden;
+		overscroll-behavior: contain;
+    }
 
+    .wheel_layout {
+        display: flex;
+        box-sizing: border-box;
+        width: 100%;
+        height: 100%;
+        justify-content: center;
+        align-items: center;
+        gap: min(5vw, 100px);
+        padding: 10px;
+    }
+
+    @media (max-width: 1000px) {
+        .wheel_layout {
+            flex-direction: column !important;
+            gap: min(5vh, 100px);
+        }
+    }
+    /* width */
+    :global(::-webkit-scrollbar) {
+        width: 5px;
+    }
+
+    /* Track */
+    :global(::-webkit-scrollbar-track) {
+        background: none;
+        border-radius: 1px;
+    }
+
+    /* Handle */
+    :global(::-webkit-scrollbar-thumb) {
+        background: rgb(48, 48, 48);
+        border-radius: 1px;
+    }
+
+    /* Handle on hover */
+    :global(::-webkit-scrollbar-thumb:hover) {
+        background: rgb(34, 34, 34);
+    }
+</style>
